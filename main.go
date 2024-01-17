2c7580c3ee123799f0e5b48ba415f073a8a195bb
vntero <shin.antero@gmail.com>
2024-01-17 19:20:25 +0100

    adds comments

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ import (
 )
 
 func main() {
+	// loads the env variables to this file
 	godotenv.Load()
 
+	// grabs the value of a specific env variable
 	OpenWeatherApiKey := os.Getenv("API_KEY")
 
+	// prints it to the console
 	fmt.Println("Api key", OpenWeatherApiKey)
 }
